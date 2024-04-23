959fcd3feca915f48098df9ea90735c219e952ce
bry0306 <37124301+bry0306@users.noreply.github.com>
2024-04-23 16:20:50 -0400

    update

--- src/components/FrameComponent.js
+++ src/components/FrameComponent.js
@@ -1,36 +1,151 @@
+import FrameComponent1 from "./FrameComponent1";
 import "./FrameComponent.css";
 
 const FrameComponent = () => {
   return (
-    <section className="frame-parent">
-      <div className="the-milk-token-is-the-heartbe-parent">
-        <h1 className="the-milk-token-container">
-          <span>{`The `}</span>
-          <span className="milk">$MILK</span>
-          <span>{` token is the heartbeat of the Milkparty ecosystem, powering the platform's features and rewarding users for their participation. By holding `}</span>
-          <span className="milk1">$MILK</span>
-          <span>
-            , users can unlock exclusive access to platform perks including
-            higher tiered profiles, rewards from platform fees, and users can
-            even copytrade friends.
-          </span>
-        </h1>
-        <h1 className="got-milk">
-          <span>{`Got `}</span>
-          <span className="milk2">$MILK?</span>
-        </h1>
+    <section className="wave-2-1-parent">
+      <img className="wave-2-1-icon" loading="lazy" alt="" src="/wave2-1.svg" />
+      <div className="rectangle-parent1">
+        <div className="frame-child1" />
+        <div className="frame-parent8">
+          <div className="rectangle-parent2">
+            <div className="frame-child2" />
+            <div className="milkparty-container">
+              <h1 className="milkparty2">
+                <span>m</span>
+                <span className="i2">i</span>
+                <span className="l2">l</span>
+                <span className="k2">k</span>
+                <span>party./</span>
+              </h1>
+            </div>
+            <FrameComponent1
+              icons8Checkmark242="/icons8checkmark24-1@2x.png"
+              icons8Checkmark2411="/icons8checkmark24-1@2x.png"
+            />
+          </div>
+          <div className="rectangle-parent3">
+            <div className="frame-child3" />
+            <div className="current-defi-dex-wrapper">
+              <h1 className="current-defi-dex">Current Defi Dex</h1>
+            </div>
+            <FrameComponent1
+              icons8Checkmark242="/icons8x30-1@2x.png"
+              icons8Checkmark2411="/icons8x30-1@2x.png"
+              propWidth="unset"
+              propAlignSelf="stretch"
+              propPadding="var(--padding-6xs) 0px 0px"
+            />
+          </div>
+        </div>
+        <div className="frame-wrapper5">
+          <div className="frame-parent9">
+            <div className="roadmap-wrapper">
+              <div className="roadmap">2024 Roadmap</div>
+            </div>
+            <h1 className="launch-marketing-campaign-container">
+              <ul className="launch-marketing-campaign-milk">
+                <li className="launch-marketing-campaign">
+                  Launch Marketing Campaign
+                </li>
+                <li className="milk-token-presale">Milk Token Presale</li>
+                <li className="integrate-launchpad-for">
+                  Integrate Launchpad for new tokens
+                </li>
+                <li className="launch-on-milkparty">
+                  Launch on Milkparty and other Dex’s
+                </li>
+                <li className="optimize-social-platform">
+                  Optimize Social platform
+                </li>
+                <li className="get-listed-on">Get listed on Cex’s</li>
+                <li className="start-platform-rewards">
+                  Start platform rewards
+                </li>
+                <li className="first-community-airdrop">
+                  First Community Airdrop
+                </li>
+                <li className="milkparty-nft-collection">
+                  Milkparty NFT collection
+                </li>
+                <li className="wallet-chrome-extension">
+                  Wallet Chrome Extension
+                </li>
+                <li className="desktop-apps-for">{`Desktop Apps for Mac & Windows`}</li>
+                <li>Mobile apps for iOS and Android </li>
+              </ul>
+            </h1>
+          </div>
+        </div>
+        <footer className="frame-footer">
+          <div className="frame-child4" />
+          <h1 className="milkparty3">
+            <span> m</span>
+            <span className="i3">i</span>
+            <span className="l3">l</span>
+            <span className="k3">k</span>
+            <span>party./</span>
+          </h1>
+          <div className="frame-parent10">
+            <div className="rectangle-parent4">
+              <div className="frame-child5" />
+              <h1 className="enter-you-email">Enter you email</h1>
+              <div className="frame-parent11">
+                <div className="sign-up-for-our-newsletter-and-wrapper">
+                  <div className="sign-up-for">
+                    Sign up for our newsletter and join the growing Milkparty
+                    community.
+                  </div>
+                </div>
+                <button className="frame-button">
+                  <div className="frame-child6" />
+                  <div className="sign-up">Sign up</div>
+                </button>
+              </div>
+            </div>
+            <div className="frame-parent12">
+              <div className="product-parent">
+                <div className="product">Product</div>
+                <div className="download-parent">
+                  <div className="download">Download</div>
+                  <div className="security">Security</div>
+                  <div className="support">Support</div>
+                  <div className="feature-requests">Feature Requests</div>
+                </div>
+              </div>
+              <div className="resources-parent">
+                <div className="resources">Resources</div>
+                <div className="explore-parent">
+                  <div className="explore">Explore</div>
+                  <div className="learn">Learn</div>
+                  <div className="newsletter">Newsletter</div>
+                  <div className="docs">Docs</div>
+                </div>
+              </div>
+              <div className="company-parent">
+                <div className="company">Company</div>
+                <div className="about-parent">
+                  <div className="about">About</div>
+                  <div className="terms">Terms</div>
+                  <div className="privacy">Privacy</div>
+                  <div className="status">Status</div>
+                  <div className="careers">Careers</div>
+                </div>
+              </div>
+              <div className="socials-parent">
+                <div className="socials">Socials</div>
+                <div className="twitter-parent">
+                  <div className="twitter">Twitter</div>
+                  <div className="discord">Discord</div>
+                  <div className="telegram">Telegram</div>
+                  <div className="instagram">Instagram</div>
+                  <div className="reddit">Reddit</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
-      <div className="milkinfo-parent">
-        <h1 className="milkinfo">What is Milkparty?</h1>
-        <h1 className="milkparty-is-a">
-          Milkparty is a decentralized exchange built on Solana that seamlessly
-          integrates a dynamic social platform. This approach empowers users to
-          Connect, Share, and Swap with their friends and the broader crypto
-          community all in one place.
-        </h1>
-      </div>
-      <img className="test-icon1" alt="" src="/test@2x.png" />
-      <img className="test-icon2" alt="" src="/test-1@2x.png" />
     </section>
   );
 };
--- src/components/FrameComponent1.js
+++ src/components/FrameComponent1.js
@@ -1,91 +1,55 @@
+import { useMemo } from "react";
 import "./FrameComponent1.css";
 
-const FrameComponent1 = () => {
+const FrameComponent1 = ({
+  icons8Checkmark242,
+  icons8Checkmark2411,
+  propWidth,
+  propAlignSelf,
+  propPadding,
+}) => {
+  const frameDivStyle = useMemo(() => {
+    return {
+      width: propWidth,
+      alignSelf: propAlignSelf,
+    };
+  }, [propWidth, propAlignSelf]);
+
+  const frameDiv1Style = useMemo(() => {
+    return {
+      padding: propPadding,
+    };
+  }, [propPadding]);
+
   return (
-    <section className="wrapper-group-3-parent">
-      <div className="wrapper-group-3">
-        <img className="wrapper-group-3-child" alt="" src="/group-3@2x.png" />
+    <div className="frame-parent7" style={frameDivStyle}>
+      <div className="frame-wrapper4" style={frameDiv1Style}>
+        <div className="icons8-checkmark-24-1-parent">
+          <img
+            className="icons8-checkmark-24-1"
+            loading="lazy"
+            alt=""
+            src="/icons8checkmark24-1@2x.png"
+          />
+          <img
+            className="icons8-checkmark-24-2"
+            loading="lazy"
+            alt=""
+            src={icons8Checkmark242}
+          />
+          <img
+            className="icons8-checkmark-24-11"
+            alt=""
+            src={icons8Checkmark2411}
+          />
+        </div>
       </div>
-      <h1 className="enjoy-group-chats-container">
-        <p className="enjoy-group">
-          <span className="span">{`               `}</span>
-          <span>Enjoy group</span>
-        </p>
-        <p className="chats-and-spread">chats and spread the memes!</p>
+      <h1 className="swaps-friends-container">
+        <p className="swaps1">Swaps</p>
+        <p className="friends-social">Friends + Social Platform</p>
+        <p className="communities1">Communities</p>
       </h1>
-      <h1 className="ape-in-with-container">
-        <p className="ape-in-with">Ape in with endless</p>
-        <p className="communities2">communities...</p>
-        <p className="free-and-paid"> free and paid.</p>
-      </h1>
-      <h1 className="swap-100s-of-container">
-        <p className="swap-100s-of">Swap 100s of tokens</p>
-        <p className="with-tools-built">{`with tools built to `}</p>
-        <p className="perform-including">{`perform, including `}</p>
-        <p className="snipe-and-copytrades">snipe and copytrades</p>
-        <p className="blank-line3">&nbsp;</p>
-        <p className="blank-line4">&nbsp;</p>
-        <p className="blank-line5">&nbsp;</p>
-        <p className="explore-defi-with">{`Explore DeFi with `}</p>
-        <p className="friends-and-some">friends and some of</p>
-        <p className="the-biggest-influencers">the biggest influencers</p>
-        <p className="in-the-market">in the market.</p>
-      </h1>
-      <img
-        className="cc-efa206e71-3-icon"
-        loading="lazy"
-        alt=""
-        src="/cc-efa206e71-3@2x.png"
-      />
-      <img
-        className="cc-efa206e71-4-icon"
-        loading="lazy"
-        alt=""
-        src="/cc-efa206e71-4@2x.png"
-      />
-      <img
-        className="cc-efa206e71-5-icon"
-        loading="lazy"
-        alt=""
-        src="/cc-efa206e71-5@2x.png"
-      />
-      <img
-        className="cc-efa206e71-6-icon"
-        loading="lazy"
-        alt=""
-        src="/cc-efa206e71-6@2x.png"
-      />
-      <img
-        className="gold-coin-money-cartoon-vector-icon"
-        loading="lazy"
-        alt=""
-        src="/goldcoinmoneycartoonvectoriconillustrationbusinessfinanceiconisolatedflatvector-13867611044-1@2x.png"
-      />
-      <img
-        className="gold-coin-money-cartoon-vector-icon1"
-        loading="lazy"
-        alt=""
-        src="/goldcoinmoneycartoonvectoriconillustrationbusinessfinanceiconisolatedflatvector-13867611044-2@2x.png"
-      />
-      <img
-        className="cb267118e1d449e5e51845ed0cdbe-icon"
-        loading="lazy"
-        alt=""
-        src="/836cb267118e1d449e5e51845ed0cdbe-1@2x.png"
-      />
-      <h1 className="milkparty-offers-the-container">
-        <p className="milkparty-offers-the">{`Milkparty offers the `}</p>
-        <p className="best-social-platform">{`best social platform in `}</p>
-        <p className="crypto">{`crypto. `}</p>
-        <p className="blank-line6">&nbsp;</p>
-        <p className="blank-line7">&nbsp;</p>
-        <p className="earn-rewards-and">Earn rewards and stay</p>
-        <p className="for-the-airdrops">for the airdrops...</p>
-        <p className="blank-line8">&nbsp;</p>
-        <p className="blank-line9">&nbsp;</p>
-        <p className="yes-tell-your">Yes, tell your friends</p>
-      </h1>
-    </section>
+    </div>
   );
 };
 
